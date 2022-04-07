67f9a8449fbbbda670265bf26d8c1d7f0973411d
PathofKaze <pathofkaze@gmail.com>
2022-04-07 22:35:31 +0800

    Add files via upload

---
+++ src/main/java/com/iss/nus/POSql/POSql/PoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.iss.nus.POSql.POSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoSqlApplication.class, args);
+	}
+
+}
